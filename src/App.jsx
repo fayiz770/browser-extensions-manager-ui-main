bbc88d7c94cb5b52be7b4c2b2b8d3726e98675f7
Waris Fayiz <mohammadwaris456@gmail.com>
2025-04-04 19:50:55 +0330

    Implement dark mode toggle functionality in App and Header components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import Main from './components/Main'
 function App() {
+  // Dark mode state
+  const [darkMode, setDarkMode] = useState(false)
+  // Toggle dark mode function
+  // This function toggles the dark mode state
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode)
+  }
+
+  const styles = {
+    backgroundColor: darkMode ? 'var(--Neutral-900)' : 'var(--Neutral-100)',
+    color: darkMode ? '#ffffff' : '#000000'
+  }
+ 
   return (
-    <div className='app'>
-      <Header />
+    <div className='app' style={styles}>
+      <Header
+        toggleDarkMode={toggleDarkMode}
+        darkMode={darkMode} 
+      />
       <Main />
     </div>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import logo from '../assets/images/logo.svg'
-import themeToggle from '../assets/images/icon-moon.svg'
-export default function Header() {
+import moon from '../assets/images/icon-moon.svg'
+import sun from '../assets/images/icon-sun.svg'
+export default function Header({toggleDarkMode, darkMode}) {
     return(
         <header
             className="header"
@@ -10,13 +11,14 @@ export default function Header() {
                 src={logo}
                 alt="Logo"
             ></img>
-            <button>
+            <button
+                onClick={toggleDarkMode}
+            >
                 <img
                     className="theme-toggle" 
-                    src={themeToggle}
+                    src={darkMode ? moon : sun}
                     alt="Toggle theme" 
                 ></img>
-
             </button>
         </header>
     )
